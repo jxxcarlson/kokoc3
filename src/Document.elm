fea27662ca0dd1dc5822c36a447c51c8e83d9fd2
James Carlson <jxxcarlson@mac.com>
2018-07-14 11:07:19 -0400

    Add encoders/decoders for Outside

--- src/Document.elm
+++ src/Document.elm
@@ -217,8 +217,6 @@ decodeChild =
         |> JPipeline.required "doc_id" (Decode.int)
         |> JPipeline.required "comment" (Decode.string)
 
-
-
 -- ENCODERS
 
 
@@ -252,7 +250,92 @@ encodeDocument document =
        
         ]
 
-        -- |> JPipeline.required "authorIdentifier" Decode.string
+decodeDocumentFromOutside : Decoder Document
+decodeDocumentFromOutside =
+    Decode.succeed Document
+        |> JPipeline.required "id" Decode.int
+        |> JPipeline.required "identifier" Decode.string
+
+        |> JPipeline.required "authorId" Decode.int
+        |> JPipeline.required "authorIdentifier" Decode.string
+        |> JPipeline.required "authorName" Decode.string
+
+        |> JPipeline.required "title" Decode.string
+        |> JPipeline.required "content" Decode.string
+        |> JPipeline.required "level" Decode.int
+
+        |> JPipeline.required "public" (Decode.bool)
+        |> JPipeline.required "access" (Decode.dict Decode.string) -- PROBLEM
+
+        |> JPipeline.required "tags" (Decode.list Decode.string) -- PROBLEM
+
+        |> JPipeline.required "children" (Decode.list decodeChild) -- PROBLEM
+        |> JPipeline.required "parentId" Decode.int
+        |> JPipeline.required "parentTitle" Decode.string
+
+        |> JPipeline.required "textType" (Decode.string |> Decode.andThen decodeTextType)
+        |> JPipeline.required "docType" (Decode.string |> Decode.andThen decodeDocType)
+        
+        |> JPipeline.required "archive" Decode.string
+        |> JPipeline.required "version" Decode.int
+
+        |> JPipeline.required "lastViewed" (Decode.map Time.millisToPosix Decode.int)
+        |> JPipeline.required "created" (Decode.map Time.millisToPosix Decode.int)
+        |> JPipeline.required "lastModified" (Decode.map Time.millisToPosix Decode.int)
+       
+        
+        
+
+encodeDocumentForOutside : Document -> Encode.Value
+encodeDocumentForOutside document =
+    Encode.object
+        [ 
+          ( "id", Encode.int <| document.id )
+        , ( "identifier", Encode.string <| document.identifier )
+
+        , ( "authorId", Encode.int <| document.authorId )
+        , ( "authorIdentifier", Encode.int <| document.authorId )
+        , ( "author_name", Encode.string <| document.authorName )
+
+        , ( "title", Encode.string <| document.title )
+        , ( "content", Encode.string <| document.content )
+        , ( "level", Encode.int <| document.level )
+
+        , ( "public", Encode.bool <| document.public )
+        , ( "access", Encode.dict identity Encode.string document.access )
+
+        , ( "tags", Encode.list Encode.string document.tags )
+
+        , ( "children", Encode.list encodeChildForOutside document.children ) 
+        , ( "parentId", Encode.int <| document.parentId )
+        , ( "parentTitle", Encode.string <| document.parentTitle )
+        
+        , ( "textType", encodeTextType <| document.textType )
+        , ( "docType", encodeDocType <| document.docType )
+        
+        , ( "archive", Encode.string <| document.archive )
+        , ( "version", Encode.int <| document.version )
+
+        , ( "lastViewed", Encode.int (Time.posixToMillis document.lastViewed) )
+        , ( "created",  Encode.int (Time.posixToMillis document.created) )
+        , ( "lastModified", Encode.int (Time.posixToMillis document.modified) )
+        
+       
+        ]
+
+
+encodeChildForOutside : Child -> Encode.Value
+encodeChildForOutside child =
+   Encode.object
+        [ 
+              ( "title", Encode.string <| child.title )
+            , ( "level", Encode.int <| child.level )
+            , ( "docIdentifier", Encode.string <| child.docIdentifier )
+            , ( "docId", Encode.int <| child.docId )
+            , ( "comment", Encode.string <| child.comment )
+        ]
+
+     -- |> JPipeline.required "authorIdentifier" Decode.string
         -- |> JPipeline.required "access" (Decode.dict Decode.string)
         -- |> JPipeline.required "children" (Decode.list decodeChild)
         -- |> JPipeline.required "lastViewed" (Decode.map Time.millisToPosix Decode.int)
