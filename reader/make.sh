5029b7f57fe3b5c36d3abd6c8eb0f3e613c3eb0a
James Carlson <jxxcarlson@mac.com>
2018-07-23 10:09:45 -0400

    Update make file and .gitignore

--- reader/make.sh
+++ reader/make.sh
@@ -12,3 +12,18 @@ echo
 echo "${color}Start web server on port 8080${reset}"
 http-server ./dist
 fi
+
+
+
+if [ "$1" = "-i" ]
+then
+echo
+echo "${color}  -i: Compile app to Main.js, use index.html${reset}"
+/Users/carlson/Downloads/2/elm make --optimize ./src/Reader.elm --output Main.js
+echo
+echo "${color}Copy files to  /usr/local/var/www/ and restart nginx${reset}"
+nginx -s stop
+cp ./dist/index.html /usr/local/var/www/
+cp ./Main.js /usr/local/var/www/
+nginx
+fi
