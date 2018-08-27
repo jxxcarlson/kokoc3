831e98c6a3ccd47b30b413da277932034b3baaa7
James Carlson <jxxcarlson@mac.com>
2018-08-27 16:07:49 -0400

    Fix uglification

--- app/deploy.sh
+++ app/deploy.sh
@@ -7,6 +7,8 @@ NGINX_REMOTE="/var/www/html/"
 DIST_LOCAL="./dist/"
 COMPILER="elm"
 
+# https://guide.elm-lang.org/optimization/asset_size.html
+
 echo
 
 if [ "$1" = "--local" ]
@@ -19,7 +21,7 @@ then
     time ${COMPILER} make --optimize ./src/Main.elm --output ${NGINX_LOCAL}Main.js
     
     echo "${color}Uglify and deploy to dist-local${reset}"
-    time uglifyjs ${NGINX_LOCAL}Main.js -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9"' -o ${NGINX_LOCAL}Main.min.js
+    time uglifyjs ${NGINX_LOCAL}Main.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=${NGINX_LOCAL}Main.min.js
     sed 's/Main.js/Main.min.js/' ./index.html > ${NGINX_LOCAL}index.html
     echo "${color}Done!${reset}"
     nginx -s reload
@@ -32,7 +34,7 @@ else
     time ${COMPILER} make --optimize ./src/Main.elm --output ${NGINX_LOCAL}Main.js
     
     echo "${color}Uglify and deploy to Digital Ocean${reset}"
-    time uglifyjs ${NGINX_LOCAL}Main.js -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9"' -o ${NGINX_LOCAL}Main.min.js
+    time uglifyjs ${NGINX_LOCAL}Main.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=${NGINX_LOCAL}Main.min.js
     scp -r ${NGINX_LOCAL}Main.min.js root@138.197.81.6:${NGINX_REMOTE}
     sed 's/Main.js/Main.min.js/' ./index.html > ${NGINX_LOCAL}index.html
     scp -r ${NGINX_LOCAL}index.html root@138.197.81.6:${NGINX_REMOTE}index.html
