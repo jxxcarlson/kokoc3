0202fcdee596b2fc325e516836bbe74e48f5710a
James Carlson <jxxcarlson@mac.com>
2018-12-07 05:35:41 -0500

    Move all UserMsg xx in module Update to module Update.User

--- app/src/Update.elm
+++ app/src/Update.elm
@@ -262,6 +262,9 @@ update msg model =
         DocMsg docMsg ->
             Update.Document.update docMsg model
 
+        UserMsg userMsg ->
+            Update.User.update userMsg model
+
         AcceptPassword str ->
             ( { model | password = str }, Cmd.none )
 
@@ -317,78 +320,6 @@ update msg model =
 
         AcceptImageName str ->
             ( { model | imageName = str }, Cmd.none )
-
-        UserMsg (ReceiveToken result) ->
-            case result of
-                Ok token ->
-                    let
-                        maybeToken =
-                            Just token
-
-                        maybeCurrentUser =
-                            User.maybeUserFromEmailAndToken model.email (User.stringFromToken token)
-
-                        bigUserCmd =
-                            case maybeCurrentUser of
-                                Nothing ->
-                                    Cmd.none
-
-                                Just user ->
-                                    Cmd.map UserMsg <| User.getBigUserRecordAtSignIn (User.userId user)
-                    in
-                        ( { model
-                            | maybeToken = maybeToken
-                            , maybeCurrentUser = maybeCurrentUser
-                            , message = "Signed in"
-                            , email = ""
-                            , password = ""
-                            , username = ""
-                          }
-                        , Cmd.batch
-                            [ Outside.sendMaybeUserDataToLocalStorage maybeCurrentUser
-                            , bigUserCmd
-                            ]
-                        )
-
-                Err err ->
-                    let
-                        errorMessage =
-                            String.trim <| HttpError.handle err
-
-                        errorResponse =
-                            if errorMessage == "Incorrect email or password" then
-                                ShowPasswordReset
-                            else if errorMessage == "Account not verified" then
-                                ShowVerifyAccount
-                            else
-                                NoErrorResponse
-                    in
-                        ( { model | message = errorMessage, errorResponse = errorResponse }, Cmd.none )
-
-        UserMsg (RespondToNewUser result) ->
-            case result of
-                Ok token ->
-                    let
-                        maybeToken =
-                            Just token
-
-                        maybeCurrentUser =
-                            User.maybeUserFromEmailAndToken model.email (User.stringFromToken token)
-                    in
-                        ( { model
-                            | maybeToken = maybeToken
-                            , maybeCurrentUser = maybeCurrentUser
-                            , message = "Signed up"
-                            , email = ""
-                            , password = ""
-                            , username = ""
-                            , currentDocument = SystemDocument.newUser
-                          }
-                        , Outside.sendMaybeUserDataToLocalStorage maybeCurrentUser
-                        )
-
-                Err err ->
-                    ( { model | message = HttpError.handle err }, Cmd.none )
 
         DocListMsg (RestoreDocumentList result) ->
             -- SET CURRENT DOCUMENT
@@ -1042,24 +973,8 @@ update msg model =
                     ( { model | message = HttpError.handle err }, Cmd.none )
 
         -- Err err -> ( { model | message = "RIL Error" }, Cmd.none )
-        UserMsg (ListUsers result) ->
-            case result of
-                Ok userList ->
-                    ( { model | userList = userList, message = "Users: " ++ (String.fromInt <| List.length userList) }, Cmd.none )
-
-                Err error ->
-                    ( { model | message = HttpError.handle error }, Cmd.none )
-
         GetUsers ->
             Search.searchForUsers model
-
-        UserMsg (AcknowledgeMediaCountIncrement result) ->
-            case result of
-                Ok reply ->
-                    ( { model | message = reply }, Cmd.none )
-
-                Err error ->
-                    ( { model | message = HttpError.handle error }, Cmd.none )
 
         MakeImage ->
             case model.maybeCurrentUser of
@@ -1133,40 +1048,6 @@ update msg model =
 
         TogglePreferencesPanel ->
             UI.togglePreferences model
-
-        UserMsg (ReceiveBigUserRecord result) ->
-            case result of
-                Ok bigUserRecord ->
-                    ( { model
-                        | blurb = bigUserRecord.user.blurb
-                        , maybeBigUser = Just bigUserRecord.user
-                      }
-                    , Cmd.none
-                    )
-
-                Err error ->
-                    ( { model | blurb = "No blurb", message = HttpError.handle error }, Cmd.none )
-
-        UserMsg (ReceiveBigUserRecordAtSignIn result) ->
-            case result of
-                Ok bigUserRecord ->
-                    ( { model
-                        | blurb = bigUserRecord.user.blurb
-                        , maybeBigUser = Just bigUserRecord.user
-                      }
-                    , Cmd.map DocListMsg (DocumentList.retrievRecentDocumentQueueFromIntListAtSignIn model.maybeCurrentUser bigUserRecord.user.documentIds)
-                    )
-
-                Err error ->
-                    ( { model | blurb = "No blurb", message = HttpError.handle error }, Cmd.none )
-
-        UserMsg (AcknowlegeBigUserUpdate result) ->
-            case result of
-                Ok bigUserRecord ->
-                    ( { model | message = "BIG USER OK" }, Cmd.none )
-
-                Err error ->
-                    ( { model | message = "BIG USER ERROR" }, Cmd.none )
 
         GetBigUser ->
             case model.maybeCurrentUser of
--- app/src/Update/User.elm
+++ app/src/Update/User.elm
@@ -1,8 +1,138 @@
 module Update.User exposing (..)
 
-import Model exposing (Model, Msg(..))
+import Model exposing (Model, Msg(..), ErrorResponse(..))
 import Queue
-import User
+import User exposing (UserMsg(..))
+import Update.HttpError as HttpError
+import Update.Outside as Outside
+import DocumentList
+import SystemDocument
+
+
+update : UserMsg -> Model -> ( Model, Cmd Msg )
+update userMsg model =
+    case userMsg of
+        ListUsers result ->
+            case result of
+                Ok userList ->
+                    ( { model | userList = userList, message = "Users: " ++ (String.fromInt <| List.length userList) }, Cmd.none )
+
+                Err error ->
+                    ( { model | message = HttpError.handle error }, Cmd.none )
+
+        AcknowledgeMediaCountIncrement result ->
+            case result of
+                Ok reply ->
+                    ( { model | message = reply }, Cmd.none )
+
+                Err error ->
+                    ( { model | message = HttpError.handle error }, Cmd.none )
+
+        ReceiveBigUserRecord result ->
+            case result of
+                Ok bigUserRecord ->
+                    ( { model
+                        | blurb = bigUserRecord.user.blurb
+                        , maybeBigUser = Just bigUserRecord.user
+                      }
+                    , Cmd.none
+                    )
+
+                Err error ->
+                    ( { model | blurb = "No blurb", message = HttpError.handle error }, Cmd.none )
+
+        ReceiveBigUserRecordAtSignIn result ->
+            case result of
+                Ok bigUserRecord ->
+                    ( { model
+                        | blurb = bigUserRecord.user.blurb
+                        , maybeBigUser = Just bigUserRecord.user
+                      }
+                    , Cmd.map DocListMsg (DocumentList.retrievRecentDocumentQueueFromIntListAtSignIn model.maybeCurrentUser bigUserRecord.user.documentIds)
+                    )
+
+                Err error ->
+                    ( { model | blurb = "No blurb", message = HttpError.handle error }, Cmd.none )
+
+        AcknowlegeBigUserUpdate result ->
+            case result of
+                Ok bigUserRecord ->
+                    ( { model | message = "BIG USER OK" }, Cmd.none )
+
+                Err error ->
+                    ( { model | message = "BIG USER ERROR" }, Cmd.none )
+
+        ReceiveToken result ->
+            case result of
+                Ok token ->
+                    let
+                        maybeToken =
+                            Just token
+
+                        maybeCurrentUser =
+                            User.maybeUserFromEmailAndToken model.email (User.stringFromToken token)
+
+                        bigUserCmd =
+                            case maybeCurrentUser of
+                                Nothing ->
+                                    Cmd.none
+
+                                Just user ->
+                                    Cmd.map UserMsg <| User.getBigUserRecordAtSignIn (User.userId user)
+                    in
+                        ( { model
+                            | maybeToken = maybeToken
+                            , maybeCurrentUser = maybeCurrentUser
+                            , message = "Signed in"
+                            , email = ""
+                            , password = ""
+                            , username = ""
+                          }
+                        , Cmd.batch
+                            [ Outside.sendMaybeUserDataToLocalStorage maybeCurrentUser
+                            , bigUserCmd
+                            ]
+                        )
+
+                Err err ->
+                    let
+                        errorMessage =
+                            String.trim <| HttpError.handle err
+
+                        errorResponse =
+                            if errorMessage == "Incorrect email or password" then
+                                ShowPasswordReset
+                            else if errorMessage == "Account not verified" then
+                                ShowVerifyAccount
+                            else
+                                NoErrorResponse
+                    in
+                        ( { model | message = errorMessage, errorResponse = errorResponse }, Cmd.none )
+
+        RespondToNewUser result ->
+            case result of
+                Ok token ->
+                    let
+                        maybeToken =
+                            Just token
+
+                        maybeCurrentUser =
+                            User.maybeUserFromEmailAndToken model.email (User.stringFromToken token)
+                    in
+                        ( { model
+                            | maybeToken = maybeToken
+                            , maybeCurrentUser = maybeCurrentUser
+                            , message = "Signed up"
+                            , email = ""
+                            , password = ""
+                            , username = ""
+                            , currentDocument = SystemDocument.newUser
+                          }
+                        , Outside.sendMaybeUserDataToLocalStorage maybeCurrentUser
+                        )
+
+                Err err ->
+                    ( { model | message = HttpError.handle err }, Cmd.none )
 
 
 updateBigUserCmd : Model -> Cmd Msg
