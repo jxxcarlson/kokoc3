8a33cdd62a620682f949f4cba15b0ce1b38882c4
James Carlson <jxxcarlson@mac.com>
2019-06-21 23:08:35 -0400

    Upggrade dependencies

--- src/MarkdownTools.elm
+++ src/MarkdownTools.elm
@@ -1,59 +1,61 @@
 module MarkdownTools exposing (view)
 
+-- import Markdown.Block as Block exposing (Block(..))
+-- import Markdown.Config exposing (Options, defaultOptions)
+-- import Markdown.Inline as Inline exposing (Inline(..))
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Markdown.Block as Block exposing (Block(..))
-import Markdown.Config exposing (Options, defaultOptions)
-import Markdown.Inline as Inline exposing (Inline(..))
+import Markdown
 import Utility
 
 
 view : String -> Html msg
 view markdownString =
-    markdownString
-        |> Utility.softBreakAlt 70
-        |> String.join "\n"
-        |> Block.parse Nothing
-        -- See answer 2 why
-        |> List.map customHtmlBlock
-        |> List.concat
-        |> div []
+    Markdown.toHtml [ class "foo" ] markdownString
 
 
-customHtmlBlock : Block b i -> List (Html msg)
-customHtmlBlock block =
-    Block.defaultHtml
-        (Just customHtmlBlock)
-        (Just customHtmlInline)
-        block
 
-
-customHtmlInline : Inline i -> Html msg
-customHtmlInline inline =
-    case inline of
-        Link url maybeTitle inlines ->
-            if String.startsWith "http://elm-lang-yada.org" url then
-                a
-                    [ href url
-                    , title (Maybe.withDefault "" maybeTitle)
-                    ]
-                    (List.map customHtmlInline inlines)
-
-            else
-                a
-                    [ href url
-                    , title (Maybe.withDefault "" maybeTitle)
-                    , target "_blank"
-                    , rel "noopener noreferrer"
-                    ]
-                    (List.map customHtmlInline inlines)
-
-        _ ->
-            Inline.defaultHtml (Just customHtmlInline) inline
-
-
-customOptions : Options
-customOptions =
-    { defaultOptions
-        | softAsHardLineBreak = True
-    }
+-- |> Utility.softBreakAlt 70
+-- |> String.join "\n"
+-- |> Block.parse Nothing
+-- -- See answer 2 why
+-- |> List.map customHtmlBlock
+-- |> List.concat
+-- |> div []
+-- customHtmlBlock : Block b i -> List (Html msg)
+-- customHtmlBlock block =
+--     Block.defaultHtml
+--         (Just customHtmlBlock)
+--         (Just customHtmlInline)
+--         block
+--
+-- customHtmlInline : Inline i -> Html msg
+-- customHtmlInline inline =
+--     case inline of
+--         Link url maybeTitle inlines ->
+--             if String.startsWith "http://elm-lang-yada.org" url then
+--                 a
+--                     [ href url
+--                     , title (Maybe.withDefault "" maybeTitle)
+--                     ]
+--                     (List.map customHtmlInline inlines)
+--
+--             else
+--                 a
+--                     [ href url
+--                     , title (Maybe.withDefault "" maybeTitle)
+--                     , target "_blank"
+--                     , rel "noopener noreferrer"
+--                     ]
+--                     (List.map customHtmlInline inlines)
+--
+--         _ ->
+--             Inline.defaultHtml (Just customHtmlInline) inline
+--
+--
+-- customOptions : Options
+-- customOptions =
+--     { defaultOptions
+--         | softAsHardLineBreak = True
+--     }
