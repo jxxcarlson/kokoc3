f3fea65e22e76e97aa38f44572bcf2d4ed70d7d2
James Carlson <jxxcarlson@mac.com>
2019-12-10 10:29:10 +0100

    user minilatex3 simpler API

--- src/DocumentListView.elm
+++ src/DocumentListView.elm
@@ -2,7 +2,7 @@ module DocumentListView exposing (DocListViewMsg(..), viewWithHeading, viewWithH
 
 import Document exposing (DocType(..), Document)
 import DocumentList exposing (DocumentList, documents)
-import MiniLatex exposing (LatexState, TocEntry, TableOfContents)
+import MiniLatex exposing (LatexState, TocEntry)
 import Element exposing (..)
 import Element.Font as Font exposing (Font)
 import Element.Input as Input
